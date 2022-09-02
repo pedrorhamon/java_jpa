9f86be51ebf3ab3feb2e11b68223b80ebdc23627
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-01 23:33:40 -0300

    começando o mapeamento da entidade

--- src/main/java/com/starking/ecommerce/EcommerceApplication.java
+++ src/main/java/com/starking/ecommerce/EcommerceApplication.java
@@ -1,13 +1,12 @@
 package com.starking.ecommerce;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class EcommerceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
-
-
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class EcommerceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(EcommerceApplication.class, args);
+//	}
+//}
