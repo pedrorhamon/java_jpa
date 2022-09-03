c260d58c84d44b7f335672ca37b31d3792ce77bf
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-03 18:35:21 -0300

    Atualizando dados com merge

---
+++ src/test/java/com/starking/ecommerce/OperacaoComTransacaoTest.java
@@ -0,0 +1,77 @@
+package com.starking.ecommerce;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Produto;
+
+public class OperacaoComTransacaoTest extends EntityManagerTest {
+	
+	@Test
+	public void atualizarObjeto() {
+		Produto produto = new Produto();
+		
+		produto.setId(1L);
+		produto.setNome("Kindle Tests");
+		produto.setDescricao("Conheça o tests");
+		produto.setPreco(new BigDecimal(4500));
+		
+		entityManager.merge(produto);
+		
+		entityManager.getTransaction().begin();
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNull(produtoVerificacao);
+		Assert.assertEquals("Kindle Tests", produtoVerificacao.getNome());
+	}
+
+	@Test
+	public void removerObjeto() {
+		Produto produto = entityManager.find(Produto.class, 1L);
+		
+		entityManager.getTransaction().begin();
+		entityManager.remove(produto);
+		entityManager.getTransaction().commit();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNull(produtoVerificacao);
+	}
+	
+	@Test
+	public void inserirOPrimeiroObjeto() {
+		Produto produto = new Produto();
+
+		produto.setId(3L);
+		produto.setNome("Câmera Canon");
+		produto.setDescricao("A melhor definição para suas fotos.");
+		produto.setPreco(new BigDecimal(5000));
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(produto);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+	}
+
+	@Test
+	public void abrirFecharATransacao() {
+//	        Produto produto = new Produto(); // Somente para o método não mostrar erros.
+
+		entityManager.getTransaction().begin();
+
+//	        entityManager.persist(produto);
+//	        entityManager.merge(produto);
+//	        entityManager.remove(produto);
+
+		entityManager.getTransaction().commit();
+	}
+}
