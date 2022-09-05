88ce57e8580ae0a05d38d289435310efe2160f9c
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-05 20:18:33 -0300

    Configurando a geração de identificador com @TableGenerator

--- src/main/java/com/starking/ecommerce/model/Categoria.java
+++ src/main/java/com/starking/ecommerce/model/Categoria.java
@@ -5,8 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,17 +19,24 @@ import lombok.Setter;
 @Table(name = "categoria")
 public class Categoria {
 
-    @EqualsAndHashCode.Include
-    @Id
+	@EqualsAndHashCode.Include
+	@Id
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq", sequenceName = "sequencias_chave_primaria")
-    private Integer id;
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+//    @SequenceGenerator(name = "seq", sequenceName = "sequencias_chave_primaria")
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+	@TableGenerator(name = "tabela", table = "hibernate_sequences", 
+		pkColumnName = "sequence_name",
+		pkColumnValue = "categoria",
+		valueColumnName = "next_val",
+		initialValue = 0,
+		allocationSize = 50)
+	private Integer id;
 
-    @Column(name = "nome")
-    private String nome;
+	@Column(name = "nome")
+	private String nome;
 
-    @Column(name = "categoria_pai_id")
-    private Integer categoriaPaiId;
+	@Column(name = "categoria_pai_id")
+	private Integer categoriaPaiId;
 
 }
