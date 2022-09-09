fb2cf4e90449907ecba5ba434b9ffce0e8bebb35
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-09 19:51:34 -0300

    funcionamento do metodo flush

---
+++ src/test/java/com/starking/ecommerce/conhecendoentitymanager/FlushTest.java
@@ -0,0 +1,29 @@
+package com.starking.ecommerce.conhecendoentitymanager;
+
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Pedido;
+import com.starking.ecommerce.model.enums.StatusPedido;
+
+public class FlushTest extends EntityManagerTest {
+
+	@Test
+	public void chamarFlush() {
+		try {
+			entityManager.getTransaction().begin();
+			Pedido pedido = entityManager.find(Pedido.class, 1);
+			pedido.setStatus(StatusPedido.PAGO);
+
+			entityManager.flush();
+			
+			if (pedido.getPagamentoCartao() == null) {
+				throw new RuntimeException("Pedido ainda não foi pago");
+			}
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			throw e;
+		}
+	}
+}
