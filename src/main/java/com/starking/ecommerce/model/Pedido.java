b4ae5f45ba235c10b366851e51d8ce72cbc14186
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-16 18:49:34 -0300

    Fazendo inserções de objetos em cascata

--- src/main/java/com/starking/ecommerce/model/Pedido.java
+++ src/main/java/com/starking/ecommerce/model/Pedido.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -42,7 +43,7 @@ public class Pedido extends EntidadeBaseInteger {
     @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name="fk_pedido_cliente"))
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "pedido")
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.PERSIST)
     private List<ItemPedido> itens;
 
     @Column(name = "data_criacao", updatable = false)
---
+++ src/test/java/com/starking/ecommerce/operacoesemcascata/CascadeTypePersistTest.java
@@ -0,0 +1,105 @@
+package com.starking.ecommerce.operacoesemcascata;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Cliente;
+import com.starking.ecommerce.model.ItemPedido;
+import com.starking.ecommerce.model.ItemPedidoId;
+import com.starking.ecommerce.model.Pedido;
+import com.starking.ecommerce.model.Produto;
+import com.starking.ecommerce.model.enums.SexoCliente;
+import com.starking.ecommerce.model.enums.StatusPedido;
+
+public class CascadeTypePersistTest extends EntityManagerTest {
+
+    // @Test
+    public void persistirPedidoComItens() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setDataCriacao(LocalDateTime.now());
+        pedido.setCliente(cliente);
+        pedido.setTotal(produto.getPreco());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setId(new ItemPedidoId());
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+        itemPedido.setQuantidade(1);
+        itemPedido.setPrecoProduto(produto.getPreco());
+
+        pedido.setItens(Arrays.asList(itemPedido)); // CascadeType.PERSIST
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedido);
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+    }
+
+    @Test
+    public void persistirItemPedidoComPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setDataCriacao(LocalDateTime.now());
+        pedido.setCliente(cliente);
+        pedido.setTotal(produto.getPreco());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setId(new ItemPedidoId());
+        itemPedido.setPedido(pedido);// Não é necessário CascadeType.PERSIST porque possui @MapsId.
+        itemPedido.setProduto(produto);
+        itemPedido.setQuantidade(1);
+        itemPedido.setPrecoProduto(produto.getPreco());
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedido);
+    }
+
+    // @Test
+    public void persistirPedidoComCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setDataNascimento(LocalDate.of(1980, 1, 1));
+        cliente.setSexoCliente(SexoCliente.MASCULINO);
+        cliente.setNome("José Carlos");
+        cliente.setCpf("01234567890");
+
+        Pedido pedido = new Pedido();
+        pedido.setDataCriacao(LocalDateTime.now());
+        pedido.setCliente(cliente); // CascadeType.PERSIST
+        pedido.setTotal(BigDecimal.ZERO);
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(cliente);
+    }
+}
