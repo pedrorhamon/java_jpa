e30e1d8ee26ff87f548157ad1553363aa143c02e
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-17 18:18:37 -0300

    remocao em cascata de manytomany

--- src/test/java/com/starking/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
+++ src/test/java/com/starking/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -1,14 +1,33 @@
 package com.starking.ecommerce.operacoesemcascata;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
 import com.starking.ecommerce.model.ItemPedido;
 import com.starking.ecommerce.model.ItemPedidoId;
 import com.starking.ecommerce.model.Pedido;
+import com.starking.ecommerce.model.Produto;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
 	
+	@Test
+	public void removerRelacaoProdutoCategoria() {
+		Produto produto = entityManager.find(Produto.class, 1);
+		
+		Assert.assertFalse(produto.getCategorias().isEmpty());
+		
+		entityManager.getTransaction().begin();
+		produto.getCategorias().clear();
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertTrue(produtoVerificacao.getCategorias().isEmpty());
+	}
+	
+	@Test
 	public void removerPedidoEItens() {
 		Pedido pedido = entityManager.find(Pedido.class, 1);
 		
