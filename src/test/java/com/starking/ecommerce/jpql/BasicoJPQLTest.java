e2e7183112764f2edaeb8f9e35226b4f9253161a
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-18 11:27:42 -0300

    persistence query language jpql

--- src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,13 +1,28 @@
 package com.starking.ecommerce.jpql;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Pedido;
 
 public class BasicoJPQLTest extends EntityManagerTest{
 
 	@Test
 	public void buscarPorIdentificador() {
+		
+		TypedQuery<Pedido> typedQuery = entityManager
+				.createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+		
+		Pedido pedido = typedQuery.getSingleResult();
+		List<Pedido> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		Assert.assertNull(pedido);
 		
 	}
 }
