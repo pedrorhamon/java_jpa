6c023ba9b8e7fb2b6e8004bc7b7c47c9a3a23991
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-18 16:03:31 -0300

    typedquery vs query

--- src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
@@ -2,6 +2,7 @@ package com.starking.ecommerce.jpql;
 
 import java.util.List;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.junit.Assert;
@@ -23,6 +24,21 @@ public class BasicoJPQLTest extends EntityManagerTest{
 		
 		Assert.assertFalse(lista.isEmpty());
 		Assert.assertNull(pedido);
+	}
+	
+	@Test
+	public void mostrarDiferencaQueries() {
+		String jpql = "select p from Pedido p where p.id = 1";
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+		Pedido pedido = typedQuery.getSingleResult();
+		Assert.assertNull(pedido);
 		
+		Query query = entityManager.createQuery(jpql);
+//		Pedido pedido2 = (Pedido) query.getSingleResult();
+//		Assert.assertNull(pedido2);
+		
+		@SuppressWarnings("unchecked")
+		List<Pedido> lista = query.getResultList();
+		Assert.assertFalse(lista.isEmpty());
 	}
 }
