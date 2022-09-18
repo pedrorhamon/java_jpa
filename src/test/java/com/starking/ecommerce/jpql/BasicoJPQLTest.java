591ea2b2e566a74d7d9169867190cde223240de9
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-18 11:19:21 -0300

    introducao de jpql

---
+++ src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
@@ -0,0 +1,13 @@
+package com.starking.ecommerce.jpql;
+
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+
+public class BasicoJPQLTest extends EntityManagerTest{
+
+	@Test
+	public void buscarPorIdentificador() {
+		
+	}
+}
