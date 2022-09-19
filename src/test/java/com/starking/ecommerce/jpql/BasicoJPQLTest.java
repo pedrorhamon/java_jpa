404556c82e6ee270e1f3a2f2ee2c2e00c58aab60
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-19 18:25:42 -0300

    Trabalhando com projeções

--- src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/starking/ecommerce/jpql/BasicoJPQLTest.java
@@ -15,6 +15,18 @@ import com.starking.ecommerce.model.Pedido;
 public class BasicoJPQLTest extends EntityManagerTest{
 	
 	@Test
+	public void projetarOResultado() {
+		String jpql = "select id, nome from Produto";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertTrue(lista.get(0).length ==2);
+		
+		lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+	}
+	
+	@Test
 	public void selecionarUmAtributoParaRetorno() {
 		String jpql = "select p.nome from Produto p";
 		
