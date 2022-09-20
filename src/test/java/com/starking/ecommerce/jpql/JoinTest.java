fb2f239df6a9cad52df015c08fe452498420ebc4
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-19 21:16:55 -0300

    Fazendo inner join entre as entidades

---
+++ src/test/java/com/starking/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,25 @@
+package com.starking.ecommerce.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+
+public class JoinTest extends EntityManagerTest{
+	
+	@Test
+	public void fazerJoin() {
+		String jpql = "select p, pag from Pedido p inner join p.pagamento pag ";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> lista = typedQuery.getResultList();
+		Assert.assertTrue(lista.size() ==1);
+		
+	}
+
+}
