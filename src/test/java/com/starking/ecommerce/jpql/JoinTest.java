c37f949188fadfd4db764328cc012df668fbb6de
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-21 19:20:50 -0300

    Fazendo o join e usando o fetch

--- src/test/java/com/starking/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/starking/ecommerce/jpql/JoinTest.java
@@ -8,17 +8,30 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Pedido;
 
 public class JoinTest extends EntityManagerTest {
 
 	@Test
+	public void usarJoinFetch() {
+		String jpql = "select p from Pedido p " + " left join fetch p.pagamento " + " join fetch p.cliente "
+				+ " left join fetch p.notaFiscal ";
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+		List<Pedido> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
+
+	@Test
 	public void fazerLeftJoin() {
-		String jpql = "select p, pag from Pedido p left join p.pagamento pag";
+		String jpql = "select p, pag from Pedido p left join p.pagamento pag on pag.status ='PROCESSANDO'";
 
 		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
 		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertTrue(lista.size() == 1);
+//		Assert.assertTrue(lista.size() == 1);
+		Assert.assertFalse(lista.isEmpty());
 	}
 
 	@Test
