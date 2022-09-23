4b63ec5d345f19785c1fd727a0da257bb17f76f8
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-23 20:52:21 -0300

    Operadores logicos

---
+++ src/test/java/com/starking/ecommerce/jpql/OperadoresLogicosTest.java
@@ -0,0 +1,25 @@
+package com.starking.ecommerce.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Pedido;
+
+public class OperadoresLogicosTest extends EntityManagerTest {
+
+    @Test
+    public void usarOperadores() {
+        String jpql = "select p from Pedido p " +
+                " where (p.status = 'AGUARDANDO' or p.status = 'PAGO') and p.total > 100";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
