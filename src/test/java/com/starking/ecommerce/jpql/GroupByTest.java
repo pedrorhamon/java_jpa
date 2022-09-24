4c2e8df7777de0dcb233e894cb65d31a92f9e641
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-24 12:21:28 -0300

    utilizando a clausula where com group by

--- src/test/java/com/starking/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/starking/ecommerce/jpql/GroupByTest.java
@@ -10,8 +10,38 @@ import org.junit.Test;
 import com.starking.ecommerce.init.EntityManagerTest;
 
 public class GroupByTest extends EntityManagerTest {
-	 @Test
-	    public void agruparResultado() {
+
+	@Test
+	public void agruparEFiltrarResultado() {
+//	         Total de vendas por mês.
+//	        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//	                " from Pedido p " +
+//	                " where year(p.dataCriacao) = year(current_date) " +
+//	                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+
+//	         Total de vendas por categoria.
+//	        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//	                " join ip.produto pro join pro.categorias c join ip.pedido p " +
+//	                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
+//	                " group by c.id";
+
+//	        Total de vendas por cliente
+		String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip "
+				+ " join ip.pedido p join p.cliente c join ip.pedido p "
+				+ " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) "
+				+ " group by c.id";
+
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+		List<Object[]> lista = typedQuery.getResultList();
+
+		Assert.assertFalse(lista.isEmpty());
+
+		lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+	}
+
+	@Test
+	public void agruparResultado() {
 //	         Quantidade de produtos por categoria.
 //	        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
 
@@ -31,20 +61,18 @@ public class GroupByTest extends EntityManagerTest {
 //	                " group by c.id";
 
 //	        Total de vendas por dia e por categoria
-	        String jpql = "select " +
-	                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
-	                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
-	                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
-	                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
-	                " order by p.dataCriacao, c.nome ";
+		String jpql = "select " + " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), "
+				+ " concat(c.nome, ': ', sum(ip.precoProduto)) "
+				+ " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c "
+				+ " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id "
+				+ " order by p.dataCriacao, c.nome ";
 
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
-	        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		List<Object[]> lista = typedQuery.getResultList();
 
-	        List<Object[]> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
 
-	        Assert.assertFalse(lista.isEmpty());
-
-	        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
-	 }
+		lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+	}
 }
