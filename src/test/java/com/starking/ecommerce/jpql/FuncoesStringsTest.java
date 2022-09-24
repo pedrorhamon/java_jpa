7aa8865bf4caa3f7e5027488f634e7427d514f2d
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-23 22:20:30 -0300

    funcoes para datas

--- src/test/java/com/starking/ecommerce/jpql/FuncoesStringsTest.java
+++ src/test/java/com/starking/ecommerce/jpql/FuncoesStringsTest.java
@@ -1,6 +1,7 @@
 package com.starking.ecommerce.jpql;
 
 import java.util.List;
+import java.util.TimeZone;
 
 import javax.persistence.TypedQuery;
 
@@ -12,7 +13,24 @@ import com.starking.ecommerce.init.EntityManagerTest;
 public class FuncoesStringsTest extends EntityManagerTest {
 	
 	@Test
-	public void paginarResultados() {
+	public void aplicarFuncaoData() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		 // current_date, current_time, current_timestamp
+        // year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
+
+        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) " +
+                " from Pedido p where hour(p.dataCriacao) > 18";
+
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+		List<Object[]> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+
+		lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+	}
+	
+	@Test
+	public void aplicarFuncaoString() {
 		 String jpql = "select c.nome, length(c.nome) from Categoria c " +
 	                " where substring(c.nome, 1, 1) = 'N'";
 
