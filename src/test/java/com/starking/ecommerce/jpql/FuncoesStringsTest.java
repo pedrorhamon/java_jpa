d3f1524c56c7aabb1f8e9e1aa4d752a118f4a513
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-24 06:18:34 -0300

    funcoes para colecoes

--- src/test/java/com/starking/ecommerce/jpql/FuncoesStringsTest.java
+++ src/test/java/com/starking/ecommerce/jpql/FuncoesStringsTest.java
@@ -13,6 +13,19 @@ import com.starking.ecommerce.init.EntityManagerTest;
 public class FuncoesStringsTest extends EntityManagerTest {
 	
 	@Test
+	public void aplicarFuncaoColecao() {
+
+        String jpql = "select size(p.itens) from Pedido p where size(p.itens) > 1";
+
+		TypedQuery<Integer> typedQuery = entityManager.createQuery(jpql, Integer.class);
+
+		List<Integer> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+
+		lista.forEach(size -> System.out.println(size));
+	}
+	
+	@Test
 	public void aplicarFuncaoNumero() {
 
         String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p " +
