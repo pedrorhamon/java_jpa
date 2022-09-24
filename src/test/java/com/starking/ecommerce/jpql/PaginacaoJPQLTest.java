412f7acf0e143ad0931105a8cbf1d4a376329e2d
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-23 21:30:15 -0300

    Fazendo paginação de resultados

---
+++ src/test/java/com/starking/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -0,0 +1,30 @@
+package com.starking.ecommerce.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Categoria;
+
+public class PaginacaoJPQLTest extends EntityManagerTest {
+
+	@Test
+	public void paginarResultados() {
+		String jpql = "select c from Categoria c order by c.nome";
+
+		TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
+
+		// FIRST_RESULT = MAX_RESULTS * (pagina - 1)
+		typedQuery.setFirstResult(6);
+		typedQuery.setMaxResults(2);
+
+		List<Categoria> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+
+		lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+	}
+}
