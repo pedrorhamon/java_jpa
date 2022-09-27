1f384536b94063b9d4363ea89d4e6efe37afa002
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-27 19:51:21 -0300

    subqueries com all

--- src/test/java/com/starking/ecommerce/jpql/SubqueriesTest.java
+++ src/test/java/com/starking/ecommerce/jpql/SubqueriesTest.java
@@ -15,6 +15,24 @@ import com.starking.ecommerce.model.Produto;
 public class SubqueriesTest extends EntityManagerTest {
 
 	@Test
+    public void pesquisarComAll() {
+        // Todos os produtos não foram vendidos mais depois que encareceram
+        String jpql = "select p from Produto p where " +
+                " p.preco > ALL (select precoProduto from ItemPedido where produto = p)";
+
+        // Todos os produtos que sempre foram vendidos pelo preco atual.
+//        String jpql = "select p from Produto p where " +
+//                " p.preco = ALL (select precoProduto from ItemPedido where produto = p)";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+    }
+	
+	@Test
     public void perquisarComExistsExercicio() {
         String jpql = "select p from Produto p " +
                 " where exists " +
