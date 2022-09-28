583fb7e70518adcc94d2b998ddfd393aa3a1a6ec
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-28 18:57:13 -0300

    atualizar em lote

--- src/test/java/com/starking/ecommerce/jpql/OperacoesEmLoteTest.java
+++ src/test/java/com/starking/ecommerce/jpql/OperacoesEmLoteTest.java
@@ -8,6 +8,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
+import javax.persistence.Query;
+
 import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
@@ -17,6 +19,19 @@ public class OperacoesEmLoteTest extends EntityManagerTest {
 
     private static final int LIMITE_INSERCOES = 4;
 
+    @Test
+    public void atualizarEmLote() {
+    	entityManager.getTransaction().begin();
+    	
+    	String jpql = "update Produto p set p.preco = p.preco + (p.preco + 0.1) where exists (select 1 from p.categorias c2 where c2.id = :categoria)";
+    	
+    	Query query = entityManager.createQuery(jpql);
+    	query.setParameter("categoria", 2);
+    	query.executeUpdate();
+    	
+    	entityManager.getTransaction().commit();
+    }
+    
     @Test
     public void inserirEmLote() throws IOException {
         InputStream in = OperacoesEmLoteTest.class.getClassLoader()
