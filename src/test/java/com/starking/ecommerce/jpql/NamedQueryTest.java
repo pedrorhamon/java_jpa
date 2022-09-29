690e62a61e605d5b12bfa091848cb76688d431e0
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-28 21:58:49 -0300

    Configurando uma query nomeada com @NamedQuery

--- src/test/java/com/starking/ecommerce/jpql/NamedQueryTest.java
+++ src/test/java/com/starking/ecommerce/jpql/NamedQueryTest.java
@@ -13,38 +13,13 @@ import com.starking.ecommerce.model.Produto;
 public class NamedQueryTest extends EntityManagerTest {
 
 	@Test
-	public void executarConsultaDinamica() {
-		Produto consultado = new Produto();
-		consultado.setNome("Câmera GoPro");
+	public void executarConsulta() {
+		TypedQuery<Produto> typedQuery = entityManager
+				.createNamedQuery("Produto.listarPorCategoria", Produto.class);
+		typedQuery.setParameter("categoria", 2);
 
-		List<Produto> lista = pesquisar(consultado);
+		List<Produto> lista = typedQuery.getResultList();
 
 		Assert.assertFalse(lista.isEmpty());
-		Assert.assertEquals("Câmera GoPro Hero 7", lista.get(0).getNome());
 	}
-
-	private List<Produto> pesquisar(Produto consultado) {
-		StringBuilder jpql = new StringBuilder("select p from Produto p where 1 = 1");
-
-		if (consultado.getNome() != null) {
-			jpql.append(" and p.nome like concat('%', :nome, '%')");
-		}
-
-		if (consultado.getDescricao() != null) {
-			jpql.append(" and p.descricao like concat('%', :descricao, '%')");
-		}
-
-		TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql.toString(), Produto.class);
-
-		if (consultado.getNome() != null) {
-			typedQuery.setParameter("nome", consultado.getNome());
-		}
-
-		if (consultado.getDescricao() != null) {
-			typedQuery.setParameter("descricao", consultado.getDescricao());
-		}
-
-		return typedQuery.getResultList();
-	}
-
 }
