7eb73aa9afffa00794e24e965c47f9cbd3e1e0c5
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-09-28 22:19:42 -0300

    Externalizando queries em um arquivo XML

--- src/test/java/com/starking/ecommerce/jpql/NamedQueryTest.java
+++ src/test/java/com/starking/ecommerce/jpql/NamedQueryTest.java
@@ -13,6 +13,16 @@ import com.starking.ecommerce.model.Produto;
 public class NamedQueryTest extends EntityManagerTest {
 
 	@Test
+    public void executarConsultaArquivoXMLEspecificoProduto() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.todos", Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+	
+	@Test
 	public void executarConsulta() {
 		TypedQuery<Produto> typedQuery = entityManager
 				.createNamedQuery("Produto.listarPorCategoria", Produto.class);
