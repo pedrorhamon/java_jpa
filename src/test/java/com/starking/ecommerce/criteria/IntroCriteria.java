3f0da31a4677a135dab01bee0302c29e39097d33
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-01 13:13:51 -0300

    IntroCriteria API do JPA

--- src/test/java/com/starking/ecommerce/criteria/IntroCriteria.java
+++ src/test/java/com/starking/ecommerce/criteria/IntroCriteria.java
@@ -1,5 +1,35 @@
 package com.starking.ecommerce.criteria;
 
-public class IntroCriteria {
+import java.util.List;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Pedido;
+
+public class IntroCriteria extends EntityManagerTest {
+
+	@Test
+	public void buscarPorIdentificador() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		
+		criteriaQuery.select(root);
+		
+		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+//		String jpql = "select sum(p.total) from Pedido p";
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+		List<Pedido> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
 }
