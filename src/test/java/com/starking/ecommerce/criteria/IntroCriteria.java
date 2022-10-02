4e6233eb2346528f8a57a70cc053d842f8ce8a62
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-02 09:44:55 -0300

    selecionando o retorno

--- src/test/java/com/starking/ecommerce/criteria/IntroCriteria.java
+++ src/test/java/com/starking/ecommerce/criteria/IntroCriteria.java
@@ -11,18 +11,34 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Cliente;
 import com.starking.ecommerce.model.Pedido;
 
 public class IntroCriteria extends EntityManagerTest {
+
+	@Test
+	public void selecionarUmAtributoParaRetorno() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+		Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+		criteriaQuery.select(root.get("cliente"));
+
+		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+		
+		TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+		Cliente cliente = typedQuery.getSingleResult();
+		Assert.assertEquals("Fernando Medeiros", cliente.getNome());
+	}
 
 	@Test
 	public void buscarPorIdentificador() {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
 		Root<Pedido> root = criteriaQuery.from(Pedido.class);
-		
+
 		criteriaQuery.select(root);
-		
+
 		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
 
 //		String jpql = "select sum(p.total) from Pedido p";
