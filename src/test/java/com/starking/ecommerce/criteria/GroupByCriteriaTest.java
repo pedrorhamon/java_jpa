6a3aae064302684c1ded6c6f79859581976424ca
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-07 18:10:28 -0300

    usando o having para condicionar o agrupamento

--- src/test/java/com/starking/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/starking/ecommerce/criteria/GroupByCriteriaTest.java
@@ -1,13 +1,18 @@
 package com.starking.ecommerce.criteria;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.TypedQuery;
-
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Root;
 
 import org.junit.Test;
 
-import javax.persistence.criteria.*;
 import com.starking.ecommerce.init.EntityManagerTest;
 import com.starking.ecommerce.model.Categoria;
 import com.starking.ecommerce.model.Categoria_;
@@ -22,6 +27,42 @@ import com.starking.ecommerce.model.Produto_;
 
 public class GroupByCriteriaTest extends EntityManagerTest {
 
+	 @Test
+	    public void condicionarAgrupamentoComHaving() {
+//	         Total de vendas dentre as categorias que mais vendem.
+//	        String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//	                " join ip.produto pro join pro.categorias cat " +
+//	                " group by cat.id " +
+//	                " having sum(ip.precoProduto) > 100 ";
+
+	        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+	        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+	        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+	        Join<ItemPedido, Produto> joinProduto = root.join(ItemPedido_.produto);
+	        Join<Produto, Categoria> joinProdutoCategoria = joinProduto.join(Produto_.categorias);
+
+	        criteriaQuery.multiselect(
+	                joinProdutoCategoria.get(Categoria_.nome),
+	                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto)),
+	                criteriaBuilder.avg(root.get(ItemPedido_.precoProduto))
+	        );
+
+	        criteriaQuery.groupBy(joinProdutoCategoria.get(Categoria_.id));
+
+	        criteriaQuery.having(criteriaBuilder.greaterThan(
+	                criteriaBuilder.avg(
+	                        root.get(ItemPedido_.precoProduto)).as(BigDecimal.class),
+	                new BigDecimal(700)));
+
+	        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+	        List<Object[]> lista = typedQuery.getResultList();
+
+	        lista.forEach(arr -> System.out.println(
+	                "Nome categoria: " + arr[0]
+	                        + ", SUM: " + arr[1]
+	                        + ", AVG: " + arr[2]));
+	    }
+	
 	@Test
     public void agruparResultadoComFuncoes() {
 //         Total de vendas por mês.
