1a2d2a9826e294f9c8c75a958012ab61e6bf2fdf
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-08 19:02:51 -0300

    Consultas nativas em java SQL

---
+++ src/test/java/com/starking/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -0,0 +1,23 @@
+package com.starking.ecommerce.consultasnativas;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+
+public class ConsultaNativaTest extends EntityManagerTest{
+	
+	@Test
+	public void executarSQL() {
+		String sql = "select * from produto";
+		Query query = entityManager.createNativeQuery(sql);
+		
+		List<Object[]> lista = query.getResultList();
+		
+		lista.stream().forEach(arry -> System.out.println(String.format("Produto => ID: %s, Nome: %s ", arry[0], arry[1])));
+	}
+
+}
