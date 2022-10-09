b0b0bb3e2212c0067128b1c4c3fa4c30d04d0e23
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-09 18:58:18 -0300

    configurando uma procedure com a anotacao namedstoredprocedurequery

--- src/test/java/com/starking/ecommerce/consultasnativas/StoredProceduresTest.java
+++ src/test/java/com/starking/ecommerce/consultasnativas/StoredProceduresTest.java
@@ -15,6 +15,17 @@ import com.starking.ecommerce.model.Cliente;
 public class StoredProceduresTest extends EntityManagerTest {
 	
 	@Test
+    public void chamarNamedStoredProcedure() {
+		StoredProcedureQuery storedProcedureQuery = entityManager.createNamedStoredProcedureQuery("compraram_acima_media");
+		
+		storedProcedureQuery.setParameter("ano", 2020);
+		
+		List<Cliente> lista = storedProcedureQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+	@Test
     public void atualizarPrecoProdutoExercicio() {
         StoredProcedureQuery storedProcedureQuery = entityManager
                 .createStoredProcedureQuery("ajustar_preco_produto", Cliente.class);
