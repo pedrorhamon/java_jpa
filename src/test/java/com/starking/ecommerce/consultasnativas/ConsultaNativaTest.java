ca85416e94459e5d3e30f5a918d6c5ee233d0121
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-09 12:53:59 -0300

    Executando SQL e retornando uma entidade

--- src/test/java/com/starking/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ src/test/java/com/starking/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -7,8 +7,30 @@ import javax.persistence.Query;
 import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Produto;
 
 public class ConsultaNativaTest extends EntityManagerTest{
+	
+	@Test
+	public void executarSQLRetornandoEntidade() {
+//      String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
+//      " from produto_loja";
+
+//String sql = "select prd_id id, prd_nome nome, prd_descricao descricao, " +
+//      "            prd_data_criacao data_criacao, prd_data_ultima_atualizacao data_ultima_atualizacao, " +
+//      "            prd_preco preco, prd_foto foto " +
+//      " from ecm_produto";
+
+		String sql = "select id, nome, descricao, " +
+                "            null data_criacao, null data_ultima_atualizacao, " +
+                "            preco, null foto " +
+                " from erp_produto";
+		Query query = entityManager.createNativeQuery(sql, Produto.class);
+		
+		List<Produto> lista = query.getResultList();
+		
+		lista.stream().forEach(obj -> System.out.println(String.format("Produto => ID: %s, Nome: %s ", obj.getId(), obj.getNome())));
+	}
 	
 	@Test
 	public void executarSQL() {
