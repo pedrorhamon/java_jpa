89e4f5cb81eb7600a4bf7492155abbc70f4a81a9
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-09 18:34:42 -0300

    invocando stored procedures com parametros in e out

---
+++ src/test/java/com/starking/ecommerce/consultasnativas/StoredProceduresTest.java
@@ -0,0 +1,28 @@
+package com.starking.ecommerce.consultasnativas;
+
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+
+public class StoredProceduresTest extends EntityManagerTest {
+	
+	@Test
+	public void usarParametrosInEOut() {
+		StoredProcedureQuery storedProcedureQuery = entityManager.createStoredProcedureQuery("buscar_nome_produto");
+		
+		storedProcedureQuery.registerStoredProcedureParameter("produto_id", Integer.class, ParameterMode.IN);
+		
+		storedProcedureQuery.registerStoredProcedureParameter("produto_nome", String.class, ParameterMode.OUT);
+		
+		storedProcedureQuery.setParameter("produto_id", 1);
+		
+		String nome = (String) storedProcedureQuery.getOutputParameterValue("produto_nome");
+		
+		Assert.assertEquals("Kindle", nome);
+	}
+
+}
