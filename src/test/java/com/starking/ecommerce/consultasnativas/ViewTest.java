e807a940164073fe1c7fbb9e48b40dc7e4d4687d
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-09 19:03:41 -0300

    invocando uma view do banco de dados

---
+++ src/test/java/com/starking/ecommerce/consultasnativas/ViewTest.java
@@ -0,0 +1,38 @@
+package com.starking.ecommerce.consultasnativas;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.junit.Test;
+
+import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Cliente;
+
+public class ViewTest extends EntityManagerTest {
+	
+	@Test
+    public void executarView() {
+        Query query = entityManager.createNativeQuery(
+                "select cli.id, cli.nome, sum(ped.total) " +
+                        " from pedido ped " +
+                        " join view_clientes_acima_media cli on cli.id = ped.cliente_id " +
+                        " group by ped.cliente_id");
+
+        List<Object[]> lista = query.getResultList();
+
+        lista.stream().forEach(arr -> System.out.println(
+                String.format("Cliente => ID: %s, Nome: %s, Total: %s", arr)));
+    }
+
+    @Test
+    public void executarViewRetornandoCliente() {
+        Query query = entityManager.createNativeQuery(
+                "select * from view_clientes_acima_media", Cliente.class);
+
+        List<Cliente> lista = query.getResultList();
+
+        lista.stream().forEach(obj -> System.out.println(
+                String.format("Cliente => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+    }
+}
