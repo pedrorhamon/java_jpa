4f45c1cf36a46c800a521924d321d2d6a2896fb7
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-09 12:57:00 -0300

    passando parametros para consulta nativa

--- src/test/java/com/starking/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ src/test/java/com/starking/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -12,6 +12,21 @@ import com.starking.ecommerce.model.Produto;
 public class ConsultaNativaTest extends EntityManagerTest{
 	
 	@Test
+	public void passarParametros() {
+		String sql = "select id, nome, descricao, " +
+                "            null data_criacao, null data_ultima_atualizacao, " +
+                "            preco, null foto " +
+                " from ecm_produto where prd_id = :id";
+		
+		Query query = entityManager.createNativeQuery(sql, Produto.class);
+		query.setParameter("id", 1);
+		
+		List<Produto> lista = query.getResultList();
+		
+		lista.stream().forEach(obj -> System.out.println(String.format("Produto => ID: %s, Nome: %s ", obj.getId(), obj.getNome())));
+	}
+	
+	@Test
 	public void executarSQLRetornandoEntidade() {
 //      String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
 //      " from produto_loja";
