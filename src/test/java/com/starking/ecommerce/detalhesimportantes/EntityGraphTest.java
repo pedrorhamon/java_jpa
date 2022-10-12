24577c80f603ec24726ccc39192f0793bae82b52
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-12 20:48:25 -0300

    Utilizando metamodel com Entity Graph

--- src/test/java/com/starking/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/starking/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -11,9 +11,34 @@ import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
 import com.starking.ecommerce.model.Cliente;
+import com.starking.ecommerce.model.Cliente_;
 import com.starking.ecommerce.model.Pedido;
+import com.starking.ecommerce.model.Pedido_;
 
 public class EntityGraphTest extends EntityManagerTest {
+	
+	@Test
+	public void buscarAtributosEssencialDePedido03() {
+		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+		entityGraph.addAttributeNodes(Pedido_.dataCriacao, Pedido_.status, Pedido_.total);
+		
+		Subgraph<Cliente> subgraphCliente = entityGraph.addSubgraph(Pedido_.cliente);
+		subgraphCliente.addAttributeNodes(Cliente_.nome, Cliente_.cpf);
+		
+//		Map<String, Object> properties = new HashMap<>();
+//		properties.put("javax.persistence.fetchgraph", entityGraph);
+//		properties.put("javax.persistence.loadgraph", entityGraph);
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+		List<Pedido> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+//		Pedido pedido = entityManager.find(Pedido.class, 1, properties);
+//		Assert.assertNotNull(pedido);
+	}
+	
 	
 	@Test
 	public void buscarAtributosEssencialDePedido02() {
