0db0fd34786be70abbb05247d666daedbed32e27
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-12 20:43:20 -0300

    adicionando um subgraph

--- src/test/java/com/starking/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/starking/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -3,15 +3,39 @@ package com.starking.ecommerce.detalhesimportantes;
 import java.util.List;
 
 import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
 import javax.persistence.TypedQuery;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.starking.ecommerce.init.EntityManagerTest;
+import com.starking.ecommerce.model.Cliente;
 import com.starking.ecommerce.model.Pedido;
 
 public class EntityGraphTest extends EntityManagerTest {
+	
+	@Test
+	public void buscarAtributosEssencialDePedido02() {
+		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+		entityGraph.addAttributeNodes("dataCriacao", "status", "total");
+		
+		Subgraph<Cliente> subgraphCliente = entityGraph.addSubgraph("cliente", Cliente.class);
+		subgraphCliente.addAttributeNodes("nome", "cpf");
+		
+//		Map<String, Object> properties = new HashMap<>();
+//		properties.put("javax.persistence.fetchgraph", entityGraph);
+//		properties.put("javax.persistence.loadgraph", entityGraph);
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+		List<Pedido> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+//		Pedido pedido = entityManager.find(Pedido.class, 1, properties);
+//		Assert.assertNotNull(pedido);
+	}
 	
 	@Test
 	public void buscarAtributosEssencialDePedido() {
