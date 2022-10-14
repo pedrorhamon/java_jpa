830a7ce20836124b8105da0f47664a69ffa96541
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-14 19:53:26 -0300

    controle dinamico do cache

--- src/test/java/com/starking/ecommerce/cache/CacheTest.java
+++ src/test/java/com/starking/ecommerce/cache/CacheTest.java
@@ -1,6 +1,10 @@
 package com.starking.ecommerce.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.Cache;
+import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -25,6 +29,36 @@ public class CacheTest {
 	public static void tearDownAfterClass() {
 		entityManagerFactory.close();
 	}
+	
+	@Test
+    public void controlarCacheDinamicamente() {
+        // javax.persistence.cache.retrieveMode CacheRetrieveMode
+        // javax.persistence.cache.storeMode CacheStoreMode
+
+        Cache cache = entityManagerFactory.getCache();
+
+        System.out.println("Buscando todos os pedidos..........................");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        entityManager1.setProperty("javax.persistence.cache.storeMode", CacheStoreMode.BYPASS);
+        entityManager1
+                .createQuery("select p from Pedido p", Pedido.class)
+                .setHint("javax.persistence.cache.storeMode", CacheStoreMode.USE)
+                .getResultList();
+
+        System.out.println("Buscando o pedido de ID igual a 2..................");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Map<String, Object> propriedades = new HashMap<>();
+//        propriedades.put("javax.persistence.cache.storeMode", CacheStoreMode.BYPASS);
+//        propriedades.put("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS);
+        entityManager2.find(Pedido.class, 2, propriedades);
+
+        System.out.println("Buscando todos os pedidos (de novo)..........................");
+        EntityManager entityManager3 = entityManagerFactory.createEntityManager();
+        entityManager3
+                .createQuery("select p from Pedido p", Pedido.class)
+//                .setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS)
+                .getResultList();
+    }
 	
 	@Test
     public void analisarOpcoesCache() {
