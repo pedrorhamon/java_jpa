435420d8b41cfbab72c476a74d69e959e10f9131
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-13 21:30:37 -0300

    removendo entidades do cache

--- src/test/java/com/starking/ecommerce/cache/CacheTest.java
+++ src/test/java/com/starking/ecommerce/cache/CacheTest.java
@@ -1,5 +1,6 @@
 package com.starking.ecommerce.cache;
 
+import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -22,6 +23,26 @@ public class CacheTest {
 	@AfterClass
 	public static void tearDownAfterClass() {
 		entityManagerFactory.close();
+	}
+	
+	@Test
+	public void removerDoCache() {
+		Cache cache = entityManagerFactory.getCache();
+
+		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+		System.out.println("Buscando a partir da instância 1:");
+		entityManager1.createQuery("select p from Pedido p", Pedido.class).getResultList();
+
+		System.out.println("Removendo do cache");
+		cache.evictAll();
+//	        cache.evict(Pedido.class);
+//	        cache.evict(Pedido.class, 1);
+
+		System.out.println("Buscando a partir da instância 2:");
+		entityManager2.find(Pedido.class, 1);
+		entityManager2.find(Pedido.class, 2);
 	}
 	
 	@Test
